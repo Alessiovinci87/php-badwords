de1f8f54b37c4525c333f4c5ba59ce27ee96bcf3
Alessiovinci87 <alessio.vinci87@gmail.com>
2023-08-21 17:39:28 +0200

    esercizio terminato

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    
+    <h2>Inserisci un paragrafo e una parola da censurare</h2>
+    <form action="process.php" method="post">
+        <label for="paragrafo">Paragrafo:</label><br>
+        <textarea id="paragrafo" name="paragrafo" rows="4" cols="50"></textarea><br><br>
+        
+        <label for="parola_censurata">Parola da censurare:</label>
+        <input type="text" id="parola_censurata" name="parola_censurata"><br><br>
+        
+        <input type="submit" value="Invia">
+    </form>
 </body>
 </html>
--- process.php
+++ process.php
@@ -4,7 +4,23 @@
     <title>PHP Badwords</title>
 </head>
 <body>
- 
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $paragrafo = $_POST["paragrafo"];
+        $parolaCensurata = $_POST["parola_censurata"];
+        
+        $paragrafoCensurato = str_ireplace($parolaCensurata, '***', $paragrafo);
+        $lunghezzaParagrafoCensuratoEffettiva = strlen($paragrafoCensurato) - (3 * substr_count($paragrafoCensurato, '***'));
+        
+        echo "<h2>Paragrafo originale</h2>";
+        echo "<p>" . $paragrafo . "</p>";
+        echo "<p>Lunghezza: " . strlen($paragrafo) . " caratteri</p>";
+        
+        echo "<h2>Paragrafo censurato</h2>";
+        echo "<p>" . $paragrafoCensurato . "</p>";
+        echo "<p>Lunghezza effettiva: " . $lunghezzaParagrafoCensuratoEffettiva . " caratteri</p>";
+    }
+    ?>
 </body>
 </html>
 
